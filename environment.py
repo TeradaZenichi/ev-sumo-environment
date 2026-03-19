250ee857ba80329037b492025e8665b1079805ea
felipe <f258434@dac.unicamp.br>
2026-03-19 16:25:56 -0300

    continuar atualizações

--- environment.py
+++ environment.py
@@ -11,23 +11,31 @@ import traci
 
 
 class SingleEV(gym.Env):
-    def __init__(self, config, vehicles, start):
+    def __init__(self, config, vehicle, start):
         super().__init__()
-        self.simulation = Sumo(config,vehicles)   # cria objeto da classe Sumo
-        self.chargers = EVSE("Charge_ParkD")  # cria objeto da classe EVSE
-        self.park = PARKING("ParkAreaC")
+        self.simulation = Sumo(config, vehicle)   # cria objeto da classe Sumo
 
+        # esse caso vai ser só um EV e colocar as rotas iniciais e finais da primeira parte no json
+        car = vehicle[list(vehicle.keys())[0]]
+        car = EV(list(vehicle.keys())[0], car["type"], [car["initial_edge"],car["routeid"], car["final_edge"]])
+        self.done = False
+        
     
     def step(self, action):
+        reward = 0
+        
         # step do ev
-
+        traci.simulationStep()
 
         # mexer na rota se for necessário
+        self.ev.step()
 
 
         # atualizar o 
-        pass
+        if traci.simulation.getTime() == self.simulation.max_time:
+            self.done = True
 
+        return # state, reward, terminated, truncated, info
 
     def get_obs(self):
         obs = []
--- mainv2.py
+++ mainv2.py
@@ -6,17 +6,22 @@ import json
 
 
 """Load config at config/config.json"""
-with open(r'config/config.json', 'r') as config_file:
+with open('data/config.json', 'r') as config_file:
     config = json.load(config_file)
 
-with open('config/vehicles.json', "r", encoding="utf-8") as f:
-    vehicles = json.load(f)
+with open('data/single_ev.json', "r", encoding="utf-8") as f:
+    vehicle = json.load(f)
 
 
 def main():
     start = "00:00"  # definir um dia, mes e ano e usar datetime
-    env = SingleEV(config, vehicles, start)
-    pass
+    env = SingleEV(config, vehicle, start)
+    
+
+    while env.done is False:
+        action = [0,0,0,0,0,0,0,1]
+        state, reward, done, truncated, info = env.step(action)
+        pass
 
 if __name__ == "__main__":
     main()
